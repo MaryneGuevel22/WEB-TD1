1bad32cfa7669be39433c2fc6411980beeece15a
MaryneGuevel22 <78147665+MaryneGuevel22@users.noreply.github.com>
2021-03-31 10:58:30 +0200

    TD1 WEB- initialisation

--- TD1/src/main/java/comptoirs/dto/UnitesParProduit.java
+++
@@ -1,9 +0,0 @@
-package comptoirs.dto;
-/*
- *Utilisé pour représenter le résultat des requêtes statistiques
- * Cette interface sera auto-implémentée par Spring
- */
-public interface UnitesParProduit {
-	String getNom();
-	Long getUnites();
-}
